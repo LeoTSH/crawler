affb126d2b0d4d2416f928e5d39fdf683aab4aac
Leo Tay <leotaysh@gmail.com>
2019-10-02 08:18:42 +0800

    Added chotot crawler

--- src/nhattao_crawler.py
+++ src/nhattao_crawler.py
@@ -7,9 +7,19 @@ class Nhattao_crawler():
         self.url = url
         self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) \
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
-        self.params = {'type':'recent', 'search_id':122705679, 'order':'up_time', 'direction':'desc'}
+        # self.headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+        self.type = 'recent'
+        self.search_id = ''
+        self.order = 'up_time'
+        self.direction = 'desc'
         self.r = requests.Session()
-        
+    
+    def _set_search_id(self):
+        page = self.r.get(self.url, headers=self.headers)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        search_id = soup.find('div', class_='PageNav')['data-baseurl'].split('/')[2]
+        self.search_id = ''.join(re.findall(r'\d', search_id))
+
     def _process_datetime(self, soup, class_):
         for dt in soup.find('li', class_=class_):
             dt_str = dt.text
@@ -83,9 +93,10 @@ class Nhattao_crawler():
 
     def get_listings_per_page(self, i):
         listing_links = []
-        url = self.url+'page-{}'.format(i) 
-        print(url)
-        page_listings = self.r.get(url, headers=self.headers, params=self.params)
+        self._set_search_id()
+        url = self.url+'page-{}?type={}&search_id={}&order={}&direction={}'.format(i, self.type, self.search_id, self.order, self.direction)
+        print('URL: {}'.format(url))
+        page_listings = self.r.get(url, headers=self.headers)
         listings = BeautifulSoup(page_listings.text, 'html.parser')
     
         # Grab all listing links from each page
